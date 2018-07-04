5e7650e2e1a80efdca4f71d49da128d1f9735bda
sajiki <k.sajiki@iinetwork.co.jp>
2018-07-04 15:08:37 +0900

    set

--- Test1/src/PushTest.java
+++ Test1/src/PushTest.java
@@ -4,4 +4,21 @@ public class PushTest {
 	public static void main(String[] args) {
 		System.out.println("pushのテストです");
 	}
+
+	public void close(){
+		System.out.println("test");
+	}
+	public void kame(){
+
+	}
+	public void test1(){
+
+	}
+	public void test2(){
+
+	}
+
+	public void hd(){
+		System.out.println();
+	}
 }
